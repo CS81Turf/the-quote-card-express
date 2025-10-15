df52d8b7bcda41a062606fa5ef86006af4cf1dc7
Chris <chrishabich3@gmail.com>
2025-10-14 23:55:39 -0400

    Created random image generator using Unsplash API

--- public/js/index.js
+++ public/js/index.js
@@ -5,36 +5,53 @@ const elements = {
     author: document.getElementById("author"),
 };
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard",       
-    },
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-    {
-        quote: "The internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt",
-    },
-    {
-        quote: "Learn to do the common things uncommonly well.",
-        author: "George Washington Carver",
-    }
-]
+async function getRandomImage() {
+    const client_id = "";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json()
+        const receivedPhotoUrl = returnedData.urls.regular;
 
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000);
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-setTimeout(loopThroughQuotes, 3000);
+getRandomImage();
+
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard",       
+//     },
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//     },
+//     {
+//         quote: "The internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+//         author: "Eric Schmidt",
+//     },
+//     {
+//         quote: "Learn to do the common things uncommonly well.",
+//         author: "George Washington Carver",
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 3000);
+// }
+
+// setTimeout(loopThroughQuotes, 3000);
